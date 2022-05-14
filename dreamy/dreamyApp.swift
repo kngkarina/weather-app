c10ed3b61480192fc8f71b9b380340ab251e8c22
Karina Ng <karinang@Karinas-MacBook-Pro.local>
2022-05-13 22:18:34 -0400

    init

---
+++ dreamy/dreamyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dreamyApp.swift
+//  dreamy
+//
+//  Created by Karina Ng on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct dreamyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
